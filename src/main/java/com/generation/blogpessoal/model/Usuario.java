ea22b543230229ab85644c389d38a9aae76c7c97
annavighi <vighianna@gmail.com>
2024-12-20 21:49:18 -0300

    Documentação da API feita com Swagger

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -28,6 +29,7 @@ public class Usuario {
 	@NotNull(message = "É obrigatório inserir o nome!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "É obrigatório inserir um email de usuário!")
 	@Email(message = "O usuário deve ser um email válido!")
 	private String usuario;
